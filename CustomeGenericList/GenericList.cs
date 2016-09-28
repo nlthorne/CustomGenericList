518ebf960013fbeaef9bb03b98c318b26d697ec8
nlthorne <nlthorne@wi.rr.com>
2016-09-28 18:11:44 -0500

    end of day, working on add/remove

---
+++ CustomeGenericList/GenericList.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace CustomeGenericList
+{
+    public class GenericList <T> : IEnumerable
+    {
+        //T value;
+        private int count = 10;
+        string toString;
+        public T[] array;
+        public int Count
+        {
+            get
+            {
+                return Count;
+            }
+            set
+            {
+                count = value;
+            }
+        }
+        public GenericList()
+        {
+            array = new T[Count];
+        }
+        public IEnumerable GetEnumerator()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                yield return array[i]; 
+            }
+        }
+        public void AddObject(T itemToAdd)
+        {
+            count++;
+        }
+        public void RemoveObject(T itemToRemove)
+        {
+            count--;
+        }
+        public override string ToString()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                toString += array[i].ToString();
+            }
+            return toString;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return array.GetEnumerator();
+        }
+    }
+
+}
--- CustomeGenericList/Program.cs
+++ CustomeGenericList/Program.cs
@@ -10,6 +10,29 @@ namespace CustomeGenericList
     {
         static void Main(string[] args)
         {
+            GenericList<int> list1 = new GenericList<int>();
+            list1.AddObject(1);
+            list1.AddObject(2);
+            list1.AddObject(3);
+            list1.AddObject(4);
+            list1.AddObject(5);
+            list1.RemoveObject(2);
+            list1.RemoveObject(4);
+            list1.ToString();
+
+            GenericList<int> list2 = new GenericList<int>();
+            list2.AddObject(1);
+            list2.AddObject(6);
+            list2.AddObject(7);
+            list2.AddObject(8);
+            list2.AddObject(9);
+            list2.AddObject(10);
+            list2.RemoveObject(6);
+            list2.RemoveObject(8);
+            list2.RemoveObject(10);
+            list2.ToString();
+
+
         }
     }
 }
